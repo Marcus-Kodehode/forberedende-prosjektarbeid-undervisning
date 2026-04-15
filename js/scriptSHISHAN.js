6f2459e1cc9371ac8594e7d68ac234c4f9cea969
joakim-jobloop <Joakim@jobloop.no>
2026-04-15 13:44:41 +0200

    removed comment in shishan file

--- js/scriptSHISHAN.js
+++ js/scriptSHISHAN.js
@@ -13,9 +13,3 @@ if (titleSHISHAN && imgSHISHANUrl && textSHISHAN && cardColorSHISHAN) {
 } else {
   document.getElementById("imgSHISHAN").src = "./images/archangel.png";
 }
-
-// const titleSHISHAN = "Willow the master";
-// const imgSHISHANUrl = "/images/SHISHAN/Barberian.png";
-// const textSHISHAN =
-//   "I am the one and this length of text is ok to have, is that ok yo?";
-// const cardColorSHISHAN = "darkred";
